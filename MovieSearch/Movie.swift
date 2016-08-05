3d6997c66dbd3f86a267257a28dbe946c72330dd
Joseph Hansen <theythemtheir@icloud.com>
2016-08-05 09:59:45 -0600

    model assembled

--- MovieSearch/Movie.swift
+++ MovieSearch/Movie.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct Movie {
+    
+    private let kTitle = "original_title"
+    private let kRating = "vote_average"
+    private let kOverview = "overview"
+    private let kPoster = "poster_path"
+    
+    let title: String
+    let rating: Float
+    let overview: String
+    let imageEndPoint: String
+    
+    init?(jsonDictionary: [String: AnyObject]) {
+        guard let title = jsonDictionary[kTitle] as? String,
+        let rating = jsonDictionary[kRating] as? Float,
+        let overview = jsonDictionary[kOverview] as? String,
+            let imageEndPoint = jsonDictionary[kPoster] as? String else { return nil }
+        
+        self.title = title
+        self.rating = rating
+        self.overview = overview
+        self.imageEndPoint = imageEndPoint
+    }
+}
