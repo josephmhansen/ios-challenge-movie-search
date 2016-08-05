97dcc002e17a9df6712c5d996a8e383cb7b6a7d2
Joseph Hansen <theythemtheir@icloud.com>
2016-08-05 09:40:57 -0600

    folder structure assembled, storyboard complete
    image and network controllers created, custom cell created, added image view and 3 labels to cell. set searchbar as delegate

---
+++ MovieSearch/MovieController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieController.swift
+//  MovieSearch
+//
+//  Created by Joseph Hansen on 8/5/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ MovieSearch/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Joseph Hansen on 8/5/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
